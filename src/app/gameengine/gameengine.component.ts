d55083ac3568bd27378b0630b06f9e59c53b1aff
todd flanders <toddfbass@gmail.com>
2021-08-02 11:02:36 -0400

    use timer to update grid

--- src/app/gameengine/gameengine.component.ts
+++ src/app/gameengine/gameengine.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GameService} from "../game.service";
+import { timer, of, Subscription} from 'rxjs';
+import {delay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-gameengine',
@@ -9,6 +11,8 @@ import {GameService} from "../game.service";
 export class GameengineComponent implements OnInit {
 
   rows: string[] = [];
+  timerSubs: Subscription | undefined
+
 
   constructor(private gameService: GameService) { }
 
@@ -23,6 +27,31 @@ export class GameengineComponent implements OnInit {
     this.gameService.setLiveCell(1,2);
 
     this.showGrid();
+    this.calculateTimeForTimer();
+  }
+
+  calculateTimeForTimer() {
+    let expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + 500); // 10 seconds from now.
+    let alertDate = new Date(expirationDate);
+    alertDate.setTime(alertDate.getTime() - 300); // 5 seconds before expiration
+    let timeDuration = (expirationDate.getTime() - alertDate.getTime());
+    this._startTimer(timeDuration);
+  }
+
+  private _startTimer(timeDuration: number | Date | undefined) {
+    if (this.timerSubs) {
+      this.timerSubs.unsubscribe();
+    }
+
+    this.timerSubs = timer(timeDuration).subscribe((data) => {
+      // make the API call, and then calculate appropriate duartion and call startTimer() again
+      of({token: Math.random()}).pipe(delay(200)).subscribe((apiData) => {
+        this.gameService.tick();
+        this.showGrid();
+        this.calculateTimeForTimer();
+      })
+    })
   }
 
 }
