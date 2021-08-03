98f61bac5b07d2300613b48d145172a802225443
todd flanders <toddfbass@gmail.com>
2021-08-03 06:41:52 -0400

    regroup tests

--- src/app/game.service.spec.ts
+++ src/app/game.service.spec.ts
@@ -6,29 +6,12 @@ function verifyGrid(expectedGrid: string[][]) {
 
 }
 
-describe('GameService', () => {
+describe('GameService Tests', () => {
   let service: GameService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(GameService);
-  });
-
-  it('kills isolated cells', () => {
-
-    service.setGrid(
-      [
-        'X.',
-        '.X'
-      ]
-    );
-
-    service.tick();
-
-    verifyGridIsEmpty();
-
-    expect(service.grid.isLive(0,0)).toBeFalse();
-    expect(service.grid.isLive(1,1)).toBeFalse();
   });
 
   it('allows re-init to an empty sized grid size', () => {
@@ -38,124 +21,6 @@ describe('GameService', () => {
       '...',
       '...'
     ])
-  });
-
-  it('allows live cells with two neighbors to survive', () => {
-    service.setLiveCell(0,0);
-    service.setLiveCell(1,0);
-    service.setLiveCell(2,0);
-
-    service.setGrid(
-      [
-        'XXX'
-      ]
-    );
-
-    displayGame();
-
-    service.tick();
-    displayGame();
-
-    expect(service.grid.isLive(1,0)).toBeTrue();
-  });
-
-  it('allows live cells with three neighbors to survive', () => {
-    service.setLiveCell(0,0);
-    service.setLiveCell(1,0);
-    service.setLiveCell(1,1);
-    service.setLiveCell(2,0);
-
-    displayGame();
-
-    service.tick();
-
-    expect(service.grid.isLive(1,0)).toBeTrue();
-  });
-
-  it('kills live cells with more than three neighbors', () => {
-    service.setLiveCell(0,0);
-    service.setLiveCell(1,0);
-    service.setLiveCell(1,1);
-    service.setLiveCell(2,0);
-    service.setLiveCell(2,1);
-
-    displayGame();
-
-    service.tick();
-
-    expect(service.grid.isLive(1,0)).toBeFalse();
-  });
-
-  it('kills live cells with less than two neighbors', () => {
-    service.setLiveCell(0,0);
-    service.setLiveCell(1,0);
-
-    displayGame();
-
-    service.tick();
-
-    expect(service.grid.isLive(1,0)).toBeFalse();
-  });
-
-  it('Any dead cell with exactly three live neighbours becomes a live cell', () => {
-    service.setLiveCell(0,0);
-    service.setLiveCell(1,0);
-    service.setLiveCell(0,1);
-
-    displayGame();
-
-    service.tick();
-
-    expect(service.grid.isLive(1,1)).toBeTrue();
-  });
-
-  it('Any dead cell with less than three live neighbours stays dead', () => {
-    service.setLiveCell(0,0);
-    service.setLiveCell(1,0);
-
-    displayGame();
-
-    service.tick();
-
-    expect(service.grid.isLive(1,1)).toBeFalse();
-  });
-
-  it('Any dead cell with more than three live neighbours stays dead', () => {
-    service.setLiveCell(0,0);
-    service.setLiveCell(1,0);
-    service.setLiveCell(0,1);
-    service.setLiveCell(2,2);
-
-    service.tick();
-
-    expect(service.grid.isLive(1,1)).toBeFalse();
-  });
-
-  it('shows a blinker', () => {
-    service.reInitEmpty(5, 5)
-
-    service.setGrid(
-      [
-        '.....',
-        '.....',
-        '.XXX.',
-        '.....',
-        '.....',
-      ]);
-
-    displayGame();
-    service.tick();
-
-    verifyGrid(
-      [
-        '.....',
-        '..X..',
-        '..X..',
-        '..X..',
-        '.....',
-
-      ]);
-
   });
 
   it('expands the border for a blinker', () => {
@@ -192,22 +57,38 @@ describe('GameService', () => {
 
   });
 
+  it('shows a blinker', () => {
+    service.reInitEmpty(5, 5)
+
+    service.setGrid(
+      [
+        '.....',
+        '.....',
+        '.XXX.',
+        '.....',
+        '.....',
+      ]);
+
+    displayGame();
+    service.tick();
+
+    verifyGrid(
+      [
+        '.....',
+        '..X..',
+        '..X..',
+        '..X..',
+        '.....',
+
+      ]);
+
+  });
+
   function displayGame() {
     let stringGrid = service.grid.asRows();
     stringGrid.forEach(function (row) {
       console.log(`|${row}|`);
     });
-  }
-
-  function verifyGridIsEmpty() {
-    if (service.grid.rowCount() == 0) return;
-    service.grid.liveRows.forEach((row, rowNum) => {
-      row.forEach((col, colNum) => {
-        expect(service.grid.isLive(colNum, rowNum))
-          .withContext(`Expected column ${colNum}, Row ${rowNum} to be dead`)
-          .toBeFalse();
-      });
-    })
   }
 
   function verifyGrid(expectedGrid: string[]) {
@@ -233,6 +114,121 @@ describe('GameService', () => {
         .withContext(`grid has ${service.grid.columnCount()} columns, expected ${expectedGrid[0].length} columns`)
         .toEqual(expectedGrid[0].length);
     }
+  }
+})
+
+describe('GameService Rules', () => {
+  let service: GameService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(GameService);
+  });
+
+  it('kills isolated cells', () => {
+
+    service.setGrid(
+      [
+        'X.',
+        '.X'
+      ]
+    );
+
+    service.tick();
+
+    verifyGridIsEmpty();
+
+    expect(service.grid.isLive(0,0)).toBeFalse();
+    expect(service.grid.isLive(1,1)).toBeFalse();
+  });
+
+  it('allows live cells with two neighbors to survive', () => {
+    service.setLiveCell(0,0);
+    service.setLiveCell(1,0);
+    service.setLiveCell(2,0);
+
+    service.setGrid(
+      [
+        'XXX'
+      ]
+    );
+
+    service.tick();
+
+    expect(service.grid.isLive(1,0)).toBeTrue();
+  });
+
+  it('allows live cells with three neighbors to survive', () => {
+    service.setLiveCell(0,0);
+    service.setLiveCell(1,0);
+    service.setLiveCell(1,1);
+    service.setLiveCell(2,0);
+
+    service.tick();
+
+    expect(service.grid.isLive(1,0)).toBeTrue();
+  });
+
+  it('kills live cells with more than three neighbors', () => {
+    service.setLiveCell(0,0);
+    service.setLiveCell(1,0);
+    service.setLiveCell(1,1);
+    service.setLiveCell(2,0);
+    service.setLiveCell(2,1);
+
+    service.tick();
+
+    expect(service.grid.isLive(1,0)).toBeFalse();
+  });
+
+  it('kills live cells with less than two neighbors', () => {
+    service.setLiveCell(0,0);
+    service.setLiveCell(1,0);
+
+    service.tick();
+
+    expect(service.grid.isLive(1,0)).toBeFalse();
+  });
+
+  it('Any dead cell with exactly three live neighbours becomes a live cell', () => {
+    service.setLiveCell(0,0);
+    service.setLiveCell(1,0);
+    service.setLiveCell(0,1);
+
+    service.tick();
+
+    expect(service.grid.isLive(1,1)).toBeTrue();
+  });
+
+  it('Any dead cell with less than three live neighbours stays dead', () => {
+    service.setLiveCell(0,0);
+    service.setLiveCell(1,0);
+
+    service.tick();
+
+    expect(service.grid.isLive(1,1)).toBeFalse();
+  });
+
+  it('Any dead cell with more than three live neighbours stays dead', () => {
+    service.setLiveCell(0,0);
+    service.setLiveCell(1,0);
+    service.setLiveCell(0,1);
+    service.setLiveCell(2,2);
+
+    service.tick();
+
+    expect(service.grid.isLive(1,1)).toBeFalse();
+  });
+
+  function verifyGridIsEmpty() {
+    if (service.grid.rowCount() == 0) return;
+    service.grid.liveRows.forEach((row, rowNum) => {
+      row.forEach((col, colNum) => {
+        expect(service.grid.isLive(colNum, rowNum))
+          .withContext(`Expected column ${colNum}, Row ${rowNum} to be dead`)
+          .toBeFalse();
+      });
+    })
   }
 
 });
