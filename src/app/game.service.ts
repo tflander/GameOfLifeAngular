d1e23a38e5982ec9ac6a34d33bb59bb45e465484
todd flanders <toddfbass@gmail.com>
2021-08-13 11:06:18 -0400

    gut the game service

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,72 +1,16 @@
 import { Injectable } from '@angular/core';
-import {FlexiGrid} from "./FlexiGrid";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  grid: FlexiGrid;
 
   constructor() {
-    this.grid = new FlexiGrid();
+    // TODO: create the game
   }
 
   tick() {
-    let updatedLiveRows = new FlexiGrid();
-    updatedLiveRows.minRow = this.grid.minRow;
-    updatedLiveRows.maxRow = this.grid.maxRow;
-    updatedLiveRows.minCol = this.grid.minCol;
-    updatedLiveRows.maxCol = this.grid.maxCol;
-    let currRow = this.grid.minRow - 1;
-    while (currRow <= this.grid.maxRow + 1) {
-      for (let col = this.grid.minCol - 1; col <= this.grid.maxCol + 1; ++col) {
-        let neighborCount = this.grid.neighborsFor(col, currRow)
-        if (this.grid.isLive(col, currRow)) {
-          if (neighborCount == 2 || neighborCount == 3) {
-            updatedLiveRows.setLiveCell(col, currRow);
-          }
-        } else {
-          if (neighborCount == 3) {
-            updatedLiveRows.setLiveCell(col, currRow);
-          }
-        }
-      }
-      ++currRow;
-    }
-    this.grid = updatedLiveRows;
-  }
-
-  setLiveCell(col: number, row: number) {
-    this.grid.setLiveCell(col, row);
-  }
-
-  reInitEmpty(columns: number, rows: number) {
-    this.grid.init(columns, rows);
-  }
-
-  reInitRandom(columns: number, rows: number) {
-    this.reInitEmpty(columns, rows);
-
-    let currRow = this.grid.minRow ;
-    while (currRow <= this.grid.maxRow) {
-      for (let col = this.grid.minCol; col <= this.grid.maxCol; ++col) {
-        if (Math.random() < 0.5) {
-          this.grid.setLiveCell(col, currRow);
-        }
-      }
-      ++currRow;
-    }
-
-  }
-
-  setGrid(gridForSetup: string[]) {
-    gridForSetup.forEach((row, rowNum) => {
-      for (let colNum = 0; colNum < row.length; colNum++) {
-        if (row[colNum] === 'X') {
-          this.grid.setLiveCell(colNum, rowNum);
-        }
-      }
-    });
+    // TODO: update the game state
   }
 
 }
